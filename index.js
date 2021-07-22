bef244ffb399e284b6d59db10d8cb28445b9f1ca
Philip <philipbdev@gmail.com>
2021-07-22 17:39:54 -0400

    Adjusted cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://myweather.city');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
