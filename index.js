3ab36844a9c7e13af137afe5f5cf560f7a99475c
Philip <philipbdev@gmail.com>
2021-07-22 14:42:45 -0400

    Adjusted cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'https://city-weather-today.herokuapp.com',
-      'https://city-weather-app-1.netlify.app',
+      'https://myweather.city',
     ],
     credentials: true,
   })
